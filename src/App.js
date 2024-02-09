13676d4980e1f34d4667062753f8e12497472f5e
Wiktor Bilnik <bilnikwiktor@gmail.com>
2024-02-09 16:39:14 +0100

    Fix: navbar  update App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import MainPage from './components/MainPage'
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Weather from './components/Weather';
 
 function App() {
-
+    const [weatherData, setWeatherData] = useState(null);
     return (
-        <div className="w-full h-screen bg-red-300 text-white flex justify-center items-center">
-            <MainPage />
+        <div className="w-full h-screen bg-red-300  flex justify-center items-center">
+            <div className="bg-black border-white w-3/4 h-3/4  flex">
+                <Navbar setWeatherData={setWeatherData} />
+                <Weather weatherData={weatherData} />
+            </div>
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,24 @@
-import React, {useState} from 'react';
+// Navbar.js
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import fetchWeatherData from '../data/api';
 
-
-
-const Navbar = () => {
-
+const Navbar = ({ setWeatherData }) => {
     const [location, setLocation] = useState('');
-    const [weatherData, setWeatherData] = useState(null);
-    const [weatherInfo, setWeatherInfo] = useState(null);
-
-
+    const [weatherData, setLocalWeatherData] = useState(null);
 
     const checkWeather = async () => {
         try {
-          const data = await fetchWeatherData(location);
-          setWeatherData(data);
-          
+            const data = await fetchWeatherData(location);
+            setWeatherData(data);
+            setLocalWeatherData(data); // Ustawienie lokalnej kopii danych pogodowych
         } catch (error) {
-          setWeatherData(null);
-          alert('Wystąpił błąd podczas pobierania danych pogodowych.');
-          console.error('Błąd:', error);
+            setWeatherData(null);
+            setLocalWeatherData(null);
+            alert('Wystąpił błąd podczas pobierania danych pogodowych.');
+            console.error('Błąd:', error);
         }
-      };
+    };
 
     return (
         <div className="w-1/3 h-full bg-gray-300 text-black flex flex-col justify-between items-center">
@@ -33,31 +29,30 @@ const Navbar = () => {
                     className="rounded-2xl  py-2  w-full focus:none"
                     value={location}
                     onChange={(e) => {
-                      setLocation(e.target.value);
+                        setLocation(e.target.value);
                     }}
                 />
                 <button
-                     type="submit"
-                     onClick={checkWeather}
+                    type="button" // zmiana na type="button"
+                    onClick={checkWeather}
                 >
                     <FaSearch size={20} className="text-blue-700"/>
                 </button>
             </div>
-
             <div className="text-center">
-            {weatherData && weatherData.current && (
-          <img
-            src={`https:${weatherData.current.condition.icon}`}
-            alt="Weather Icon"
-            className="w-32 h-32 object-cover rounded-full mx-auto"
-          />
-        )}
-            <h3 className="text-7xl font-bold">{weatherData && weatherData.current && weatherData.current.temp_c}°C </h3>
+                {weatherData && weatherData.current && (
+                    <img
+                        src={`https:${weatherData.current.condition.icon}`}
+                        alt="Weather Icon"
+                        className="w-32 h-32 object-cover rounded-full mx-auto"
+                    />
+                )}
+                <h3 className="text-7xl font-bold">{weatherData && weatherData.current && weatherData.current.temp_c}°C </h3>
                 <div className="text-2xl text-gray-500">{weatherData && weatherData.current && weatherData.current.condition.text}</div>
             </div>
 
             <div className="text-center my-4">
-            {weatherData && weatherData.location && weatherData.location.localtime}
+                {weatherData && weatherData.location && weatherData.location.localtime}
             </div>
         </div>
     );
